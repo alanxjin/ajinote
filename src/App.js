3f100f270ece6520b8890049005011c17d531a88
ajin <ajin@trustwave.com>
2018-07-17 13:16:22 -0500

    Use draftjs, and enable markdown and math-latex

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React, { Component } from 'react';
 import './App.css';
-import SplitPane from 'react-split-pane';
-import Editor from './editor.js';
-import ReactMarkdown from 'react-markdown';
+import React, { Component } from 'react';
+import Editor from 'draft-js-plugins-editor';
+import {EditorState} from 'draft-js';
+import createMarkdownPlugin from 'draft-js-markdown-plugin';
+import createMathjaxPlugin from 'draft-js-mathjax-plugin'
 
+const plugins = [createMarkdownPlugin(),createMathjaxPlugin()];
 class App extends Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {
-      markdownSrc: "# Hello World",
+      editorState: EditorState.createEmpty(),
     }
-    this.onMarkdownChange = this.onMarkdownChange.bind(this);
+  
   }
-  onMarkdownChange(md) {
-    this.setState({
-      markdownSrc: md
-    });
+  onChange = (editorState) =>{
+    this.setState({editorState});
   }
+
   render() {
     return (
       <div className="App">
-        <SplitPane split="vertical" defaultSize="50%">
-          <div className="editor-pane">
-            <Editor className="editor" value={this.state.markdownSrc} onChange={this.onMarkdownChange}/>
-          </div>
-          <div className="view-pane">
-            <ReactMarkdown className="result" source={this.state.markdownSrc} />
-          </div>
-        </SplitPane>
+        <Editor editorState={this.state.editorState}  
+                onChange={this.onChange}
+                plugins={plugins} />
       </div>
+   
     );
   }
 }
