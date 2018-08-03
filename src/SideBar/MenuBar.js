56cf74cc780f0174dcea483035a279bf189304be
ajin <xilunjin7@gmail.com>
2018-08-03 16:50:04 -0500

    Add button for the MenuBar

--- src/SideBar/MenuBar.js
+++ src/SideBar/MenuBar.js
@@ -3,17 +3,19 @@ import DeleteOutlinedIcon from '@material-ui/icons/DeleteOutlined';
 import NoteAddOutlinedIcon from '@material-ui/icons/NoteAddOutlined';
 import CreateNewFolderOutlinedIcon from '@material-ui/icons/CreateNewFolderOutlined';
 import RefreshIcon from '@material-ui/icons/Refresh'
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 
 class MenuBar extends Component {
 
   render() {
     
     return (
-      <div className="MenuBar">
-        <NoteAddOutlinedIcon></NoteAddOutlinedIcon>
-        <CreateNewFolderOutlinedIcon></CreateNewFolderOutlinedIcon>
-        <DeleteOutlinedIcon></DeleteOutlinedIcon>
-        <RefreshIcon></RefreshIcon>
+      <div className="MenuBar" style={{margin:"5px"}}>
+        <IconButton><NoteAddOutlinedIcon></NoteAddOutlinedIcon></IconButton>
+        <IconButton><CreateNewFolderOutlinedIcon></CreateNewFolderOutlinedIcon></IconButton>
+        <IconButton><DeleteOutlinedIcon></DeleteOutlinedIcon></IconButton>
+        <IconButton><RefreshIcon></RefreshIcon></IconButton>
       </div>
     );
   }
