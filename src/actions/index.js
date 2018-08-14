8e3ccc47bd8b2d07f8f08360bd0a38a7a9a8d285
ajin <xilunjin7@gmail.com>
2018-08-14 16:26:30 -0500

    Add actions

---
+++ src/actions/index.js
@@ -0,0 +1,29 @@
+import Util from '../utility/util';
+
+let testId = 0
+
+//generateUniqueId = () => {
+//    Util.generateId
+//}
+
+export const addFile = name => ({
+    type: 'ADD_FILE',
+    id: testId++,
+    name
+})
+
+export const addFolder = name =>({
+    type: 'ADD_FOLDER',
+    id: testId++,
+    name
+})
+
+export const deleteItem = id =>({
+    type: 'DELETE_ITEM',
+    id
+})
+
+export const toggleFolder = id => ({
+    type: 'TOGGLE_FOLDER',
+    id
+})
