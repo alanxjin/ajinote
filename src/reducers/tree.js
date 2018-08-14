42591d636296eee7f536a3a17cf02d531f503301
ajin <xilunjin7@gmail.com>
2018-08-14 16:51:40 -0500

    Add reducer for tree

---
+++ src/reducers/tree.js
@@ -0,0 +1,20 @@
+
+const tree = (state=[], action) =>{
+    switch(action.type){
+        case 'ADD_FILE':
+            return  "";
+        case 'ADD_FOLDER':
+            return "";
+        case 'DELETE_ITEM':
+            return "";
+        case 'RENAME_ITEM':
+            return "";
+        case 'TOGGLE_FOLDER':
+            return "";
+        default:
+            return state
+    }
+
+}
+
+export default tree
